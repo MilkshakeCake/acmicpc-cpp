6ba6ce10a0289b6f4d97f66d979fa9e743ab213b
MilkshakeCake <olleme@gmail.com>
2022-08-31 15:50:06 +0900

    220831 2164 make commit

---
+++ Silver/2164/2164.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <cmath>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <utility>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    
+    
+}
