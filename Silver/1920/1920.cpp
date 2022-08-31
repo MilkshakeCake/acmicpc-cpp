5c47b2a7cd7f72898f14f712ec9e8978b23e7b0b
MilkshakeCake <olleme@gmail.com>
2022-08-31 15:48:33 +0900

    220831 1920 complete commit

---
+++ Silver/1920/1920.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <cmath>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <utility>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int n;
+    vector<int> vec;
+    cin >> n;
+    int temp;
+    for(int i = 0; i < n; i++) {
+        cin >> temp;
+        vec.push_back(temp);
+    }
+    sort(vec.begin(), vec.end());       //sort 안하면 binary_search가 제대로 작동 안함 but 시간초과
+    int m;
+    cin >> m;
+    for(int i = 0; i < m; i++) {
+        cin >> temp;
+        cout << binary_search(vec.begin(), vec.end(), temp) << '\n';
+    }
+}
