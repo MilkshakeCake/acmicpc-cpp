fbcf32c6ae65e70915a305f7e3832e2a6e7e7ec7
MilkshakeCake <olleme@gmail.com>
2022-08-31 14:59:34 +0900

    220831 9184 complete commit

--- Silver/9184/9184.cpp
+++ Silver/9184/9184.cpp
@@ -11,22 +11,28 @@ int arr[51][51][51] = {1};
 
 int w(int a, int b, int c) {
     if(a <= 0 || b <= 0 || c <= 0) {
-        arr[a][b][c] = 1;
+        if(a == 0 && b == 0 && c == 0) {
+            arr[a][b][c] = 1;
+        }
         return 1;
     }
 
-    if(a > 20 || b > 20 || c > 20) {
-        arr[a][b][c] = arr[20][20][20];
+    if(arr[a][b][c] != 0) {
         return arr[a][b][c];
     }
 
-    if(a < b && b < c) {
-        arr[a][b][c] = w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c); //arr[a][b][c-1] + arr[a][b-1][c-1] - arr[a][b-1][c];
+    else if(a > 20 || b > 20 || c > 20) {
+        arr[a][b][c] = 1048576;
+        return arr[a][b][c];
+    }
+
+    else if(a < b && b < c) {
+        arr[a][b][c] = w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c);
         return arr[a][b][c];
     }
 
     else {
-        arr[a][b][c] = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1); //arr[a-1][b][c] + arr[a-1][b-1][c] + arr[a-1][b][c-1] - arr[a-1][b-1][c-1];
+        arr[a][b][c] = w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1);
         return arr[a][b][c];
     }
 }
@@ -41,7 +47,7 @@ int main() {
             break;
         }
 
-        cout << w(a, b, c);
-    }   
+        cout << "w(" << a << ", " << b << ", " << c << ") = " << w(a, b, c) << endl;
+    }
     return 0;
 }
