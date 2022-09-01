4203d87936b6931b34b7f689c139db9d8f1e72d1
MilkshakeCake <olleme@gmail.com>
2022-09-01 15:35:15 +0900

    220901 11050 complete commit

---
+++ Bronze/11050/11050.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <cmath>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <utility>
+
+using namespace std;
+
+int arr[100][100];
+int binomial(int n, int k) {
+    if(n <= 1 || k == 0) {
+        if(k > n) {
+            arr[n][k] = 0;
+            return 0;
+        }
+        arr[n][k] = 1;
+        return 1;
+    }
+    else {
+        arr[n][k] = binomial(n-1, k-1) + binomial(n-1, k);
+        return arr[n][k];
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int n, k;
+    cin >> n >> k;
+    cout << binomial(n, k);
+}
