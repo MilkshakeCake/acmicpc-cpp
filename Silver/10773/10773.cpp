c5262f48e40521c462174e6dbfd1978d0790cd11
MilkshakeCake <olleme@gmail.com>
2022-09-05 16:31:53 +0900

    220905 10773 complete commit

---
+++ Silver/10773/10773.cpp
@@ -0,0 +1,30 @@
+// 제로 - Baekjoon Online Judge no.10773
+
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int k;
+    cin >> k;
+
+    int temp;
+    stack<int> stk;
+    for(int i = 0; i < k; i++) {
+        cin >> temp;
+        temp ? stk.push(temp) : stk.pop();
+    }
+
+    int sum = 0;
+    while(stk.size()) {
+        sum += stk.top();
+        stk.pop();
+    }
+
+    cout << sum;
+}
