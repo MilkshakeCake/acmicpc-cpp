b85bb738c65b753dea97a4e19ab334206748ff56
MilkshakeCake <olleme@gmail.com>
2022-09-05 10:35:23 +0900

    220905 4949 make commit

---
+++ Silver/4949/4949.cpp
@@ -0,0 +1,21 @@
+// 균형잡힌 세상 - Baekjoon Online Judge no.4949
+
+#include <string>
+#include <cmath>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <utility>
+#include <queue>
+#include <stack>
+#include <deque>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    
+}
