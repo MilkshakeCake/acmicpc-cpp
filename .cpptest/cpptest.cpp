d7b0e8b3d67c054f97d64efc23eb7ec36579133d
MilkshakeCake <olleme@gmail.com>
2022-09-08 10:20:19 +0900

    220908 1074 memory commit

--- .cpptest/cpptest.cpp
+++ .cpptest/cpptest.cpp
@@ -14,12 +14,5 @@
 using namespace std;
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    
-    map<int, int> myM;
-    myM.insert(make_pair(1, 2));
-    myM[2]++;
-    cout << myM[2];
+    cout << 2*pow(31, 3);
 }
--- Silver/1074/1074.cpp
+++ Silver/1074/1074.cpp
@@ -1,78 +1,35 @@
 // Z - Baekjoon Online Judge no.1074
 
-#include <string>
 #include <cmath>
-#include <vector>
 #include <iostream>
 #include <algorithm>
-#include <utility>
-#include <queue>
-#include <stack>
-#include <deque>
 
 using namespace std;
 
-int big = 0;
-int small = 0;
-int bigger(int a, int b) {
-    big = a >= b ? a : b;
-    return big;
-}
-int smaller(int a, int b) {
-    small = a <= b ? a : b;
-    return small;
-}
+int result = 0;
 
-vector<vector<int> > v;
+int r, c;
 
-int recursive(int r, int c, vector<vector<int> > v) {
-    int** pt;
-    if(r < 2 && c < 2) {
-        v[r][c] = 2 * r + c;
-        return v[r][c];
+void recursive(int x, int y, int n) {
+    if(x == r && y == c) {
+        cout << result;
+        return;
     }
 
-    int i1 = 1;
-    while(r != r % i1) i1 *= 2;
-    int i2 = 1;
-    while(c != c % i2) i2 *= 2;
-
-    int b = bigger(i1, i2);
-    int s = smaller(i1, i2);
-    
-    if(s == b) {
-
+    if(r >= x + n || c >= y + n) {
+        result += n * n;
+        return;
     }
-    else if(s <= b / 2) {
-        if(i1 == s){
-            v[r][c] = recursive(r, c-b/2, v) + b * b / 4;
-        }
-        else if(i2 == s){
-            v[r][c] = recursive(r-b/2, c, v) + b * b / 2;
-        }
-    }
+
+    recursive(x, y, n/2);
+    recursive(x, y + n/2, n/2);
+    recursive(x + n/2, y, n/2);
+    recursive(x + n/2, y + n/2, n/2);
 }
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    
-    int n, r, c;
+    int n;
     cin >> n >> r >> c;
-    int i1 = 1;
-    int i2 = 1;
-    while(r != r % i1) {
-        i1 *= 2;
-    }
-    while(c != c % i2) {
-        i2 *= 2;
-    }
 
-    int* arr[bigger(i1, i2)];
-    for(int i = 0; i < big; i++) {
-        arr[i] = new int[big];
-    }
-
-
+    recursive(0, 0, pow(2, n)); 
 }
