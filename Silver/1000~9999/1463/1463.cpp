554446e4c8ca4cbac449b0fcc17f19139176c4a6
MilkshakeCake <olleme@gmail.com>
2022-09-15 17:54:56 +0900

    220915 1463 complete commit

---
+++ Silver/1000~9999/1463/1463.cpp
@@ -0,0 +1,49 @@
+// 1로 만들기 - Baekjoon Online Judge no.1463
+
+#include <string>
+#include <cmath>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <utility>
+#include <queue>
+#include <stack>
+#include <deque>
+
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define fr first
+#define sc second
+#define vt vector
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
+
+#define F_OR(i, a, b, s) for (int i = (a); (s) > 0 ? i < (b) : i > (b); i += (s))
+#define F_OR1(e) F_OR(i, 0, e, 1)
+#define F_OR2(i, e) F_OR(i, 0, e, 1)
+#define F_OR3(i, b, e) F_OR(i, b, e, 1)
+#define F_OR4(i, b, e, s) F_OR(i, b, e, s)
+#define EACH(x, a) for (auto& x : a)
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int n;
+    cin >> n;
+    vt<int> dp(n+1, 0);
+    dp[1] = 0;
+
+    for(int i = 2; i <= n; i++) {
+        dp[i] = dp[i-1] + 1;
+        if(!(i%3)) dp[i] = min(dp[i], dp[i/3] + 1);
+        if(!(i%2)) dp[i] = min(dp[i], dp[i/2] +1);
+    }
+    cout << dp[n];
+}
