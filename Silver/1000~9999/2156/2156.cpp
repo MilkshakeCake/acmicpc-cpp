b74016f353fd302159f868de9e70a53f7643cfde
MilkshakeCake <olleme@gmail.com>
2022-09-19 08:55:24 +0900

    220919 2156 complete commit

---
+++ Silver/1000~9999/2156/2156.cpp
@@ -0,0 +1,56 @@
+// 포도주 시식 - Baekjoon Online Judge no.2156
+
+#include <string>
+#include <cmath>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <utility>
+#include <queue>
+#include <stack>
+#include <deque>
+
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define fr first
+#define sc second
+#define vt vector
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
+
+#define F_OR(i, a, b, s) for (int i = (a); (s) > 0 ? i < (b) : i > (b); i += (s))
+#define F_OR1(e) F_OR(i, 0, e, 1)
+#define F_OR2(i, e) F_OR(i, 0, e, 1)
+#define F_OR3(i, b, e) F_OR(i, b, e, 1)
+#define F_OR4(i, b, e, s) F_OR(i, b, e, s)
+#define EACH(x, a) for (auto& x : a)
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int n;
+    cin >> n;
+
+    int wine[10001] = {0};
+    for(int i = 0; i < n; i++) {
+        cin >> wine[i];
+    }
+    wine[n] = 0;
+
+    int dp[10001] = {0};
+    dp[0] = wine[0];
+    dp[1] = wine[1] + wine[0];
+    dp[2] = wine[2] + wine[0];
+    for(int i = 3; i <= n; i++) {
+        dp[i] = wine[i] + max(dp[i-3] + wine[i-1], dp[i-2]);
+        dp[i] = max(dp[i], dp[i-1]);
+    }
+    cout << dp[n];
+}
