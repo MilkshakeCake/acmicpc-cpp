046afe20e6fae40f146a9a8b351595407fbe86cb
MilkshakeCake <olleme@gmail.com>
2022-09-20 13:30:31 +0900

    220920 12865 complete commit

--- Silver/10000~19999/12865/12865.cpp
+++ Silver/10000~19999/12865/12865.cpp
@@ -1,4 +1,4 @@
-// "Project_Name" - Baekjoon Online Judge no."Project_Num"
+// 평범한 배낭 - Baekjoon Online Judge no.12865
 
 #include <string>
 #include <cmath>
@@ -9,7 +9,6 @@
 #include <queue>
 #include <stack>
 #include <deque>
-#include <fstream>
 
 #define ll long long
 #define ull unsigned long long
@@ -30,31 +29,45 @@
 
 using namespace std;
 
+int board[101][100001] = {};
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
     
-    vt<int> dpv;
-    vt<int> dpw;
-    vt<pii> arr;
+    vt<pii> arr(101);
 
     int n, k;
     cin >> n >> k;
-    int temp1, temp2;
     for(int i = 0; i < n; i++) {
-        cin >> temp1 >> temp2;
-        arr.push_back(make_pair(temp1, temp2));
+        cin >> arr[i].fr >> arr[i].sc;
     }
 
-    dpw[0] = arr[0].sc;
+    for(int i = arr[0].fr; i <= k; i++) {
+        board[0][i] = arr[0].sc;
+    }
 
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < i; j++) {
-            if(dpw[j] + arr[i].sc <= k) {
-                dpw[i] = dpw[j] + arr[i].sc;
+    for(int i = 1; i < n; i++) {
+        int w = arr[i].fr;
+        int v = arr[i].sc;
+        board[i][w-1] = v;
+
+        for(int j = 1; j <= k; j++) {
+            if(j < w) {
+                board[i][j] = board[i-1][j];
             }
-            else dpw[i] = arr[i].sc;
+            else {
+                board[i][j] = max(board[i-1][j], v + board[i-1][j - w]);
+            }
         }
     }
+
+    int result = 0;
+
+    for(int i = 0; i < n; i++) {
+        result = max(result, board[i][k]);
+    }
+
+    cout << result;
 }
