9e52b695cf58e1890e1c713ddb2a707d25bac9ea
MilkshakeCake <olleme@gmail.com>
2022-09-21 17:26:11 +0900

    220921 10986 complete commit

--- 3.Gold/10000~19999/10986/10986.cpp
+++ 3.Gold/10000~19999/10986/10986.cpp
@@ -35,5 +35,19 @@ int main() {
     cin.tie(NULL);
     cout.tie(NULL);
     
-    
+    int n, mod;
+    cin >> n >> mod;
+    vt<int> arr(mod, 0);
+
+
+    int temp;
+    unsigned ll int sum = 0;
+    unsigned ll int result = 0;
+    for(int i = 0; i < n; i++) {
+        cin >> temp;
+        sum += temp;
+        sum %= mod;
+        result += arr[sum % mod]++;
+    }
+    cout << result + arr[0];
 }
