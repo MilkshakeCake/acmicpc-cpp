63f192d0e7e0a32cac7ab6fb6d67dfbaedea961a
MilkshakeCake <ollehme@gmail.com>
2022-09-29 19:32:46 +0900

    220929 final commit

--- CodeTest/851/M/M.cpp
+++ CodeTest/851/M/M.cpp
@@ -14,8 +14,7 @@
 
 using namespace std;
 
-vector<int> play(100000, 0);
-bool prev[100000] = {false};
+int play[100000];
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -24,25 +23,15 @@ int main() {
 
     int n;
     int sum = 0;
+    int maxn = 0;
     cin >> n;
     for(int i = 0; i < n; i++) {
         cin >> play[i];
         sum += play[i];
+        maxn = max(maxn, play[i]);
     }
 
-    sort(play.begin(), play.end(), greater<>());
+    sum -= maxn;
 
-    int elsenum = sum - play[0];
-
-    if(play[0] > (elsenum + 1)) {
-        cout << ((elsenum) * 2) + 1;
-    }
-
-    else if(play[0] == (elsenum + 1) || play[0] * 2 == sum) {
-        cout << sum;
-    }
-
-    else {
-        
-    }
+    cout << min(maxn, sum + 1) + sum;
 }
