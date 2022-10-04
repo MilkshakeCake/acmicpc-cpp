332664efd045ef7b208a6b2006d1ff41c304cd0b
MilkshakeCake <olleme@gmail.com>
2022-10-04 11:17:04 +0900

    221004 1021 complete commit

--- 2.Silver/1000~9999/1021/1021.cpp
+++ 2.Silver/1000~9999/1021/1021.cpp
@@ -30,17 +30,76 @@
 
 using namespace std;
 
+deque<int> DQ;
+
+void rotateL() {
+    DQ.push_back(DQ.front());
+    DQ.pop_front();
+}
+
+void rotateR() {
+    DQ.push_front(DQ.back());
+    DQ.pop_back();
+}
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
     
-    int n, m;
-    cin >> n >> m;
+    int N, M;
+    cin >> N >> M;
 
-    
+int left, right;
+	int cnt = 0;
+	
+	for(int i = 1; i <= N; i++){
+		DQ.push_back(i);
+	}
+	
 
-    for(int i = 0; i < m; i++) {
+	while(M--){
+		int num;
+		cin >> num;
+		
+		for(int i = 0; i < DQ.size(); i++){
+			
+			if(DQ[i] == num){
+				left = i;
+				right = DQ.size() - i;
+				break;
+				
+			}
+		}
+		
+		if(left <= right) {
+			while(true) {
+				if(DQ.front() == num) break;
 
-    }
+				DQ.push_back(DQ.front());
+				DQ.pop_front();
+				cnt++;
+				
+			}
+			DQ.pop_front();
+		}
+		
+		else{
+			cnt++;
+			while(true) {
+				if(DQ.back() == num) break;
+	
+				DQ.push_front(DQ.back());
+				DQ.pop_back();
+				cnt++;
+				
+			}
+			DQ.pop_back();
+		}
+
+	}
+	
+	cout << cnt;
+	
+	return 0;
 }
