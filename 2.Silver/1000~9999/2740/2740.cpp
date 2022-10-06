5fc5e7d540b62798dea0e2c3afad0aa67a0c7903
MilkshakeCake <olleme@gmail.com>
2022-10-06 12:58:36 +0900

    221006 2740 complete commit

---
+++ 2.Silver/1000~9999/2740/2740.cpp
@@ -0,0 +1,82 @@
+// 행렬 곱셈 - Baekjoon Online Judge #2740
+
+#include <string>
+#include <cmath>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <utility>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <fstream>
+#include <sstream>
+#include <map>
+
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define fr first
+#define sc second
+#define vt vector
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
+
+#define F_OR(i, a, b, s) for (int i = (a); (s) > 0 ? i < (b) : i > (b); i += (s))
+#define F_OR1(e) F_OR(i, 0, e, 1)
+#define F_OR2(i, e) F_OR(i, 0, e, 1)
+#define F_OR3(i, b, e) F_OR(i, b, e, 1)
+#define EACH(x, a) for (auto& x : a)
+
+using namespace std;
+
+vt<vt<int> > matrixA;
+vt<vt<int> > matrixB;
+
+int N, M, K;
+
+int matrix(int x, int y) {
+    int result = 0;
+    for(int i = 0; i < M; i++) {
+        result += matrixA[x][i] * matrixB[i][y];
+    }
+    return result;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    cin >> N >> M;
+    
+    for(int i = 0; i < N; i++) {
+        vt<int> vTemp;
+        int temp;
+        for(int j = 0; j < M; j++) {
+            cin >> temp;
+            vTemp.push_back(temp);
+        }
+        matrixA.push_back(vTemp);
+    }
+
+    cin >> M >> K;
+    for(int i = 0; i < M; i++) {
+        vt<int> vTemp;
+        int temp;
+        for(int j = 0; j < K; j++) {
+            cin >> temp;
+            vTemp.push_back(temp);
+        }
+        matrixB.push_back(vTemp);
+    }
+
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < K; j++) {
+            cout << matrix(i, j) << ' ';
+        }
+        cout << '\n';
+    }
+}
