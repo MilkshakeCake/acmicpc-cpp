597a4f934848b8390c3c5eb0bc3f30faac0763dd
MilkshakeCake <olleme@gmail.com>
2022-10-12 11:27:06 +0900

    221012 11286 complete commit

---
+++ 2.Silver/10000~19999/11286/11286.cpp
@@ -0,0 +1,73 @@
+// "Project_Name" - Baekjoon Online Judge #
+
+#include <string>
+#include <cmath>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <utility>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <fstream>
+#include <sstream>
+#include <map>
+
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define fr first
+#define sc second
+#define vt vector
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
+
+#define F_OR(i, a, b, s) for (int i = (a); (s) > 0 ? i < (b) : i > (b); i += (s))
+#define F_OR1(e) F_OR(i, 0, e, 1)
+#define F_OR2(i, e) F_OR(i, 0, e, 1)
+#define F_OR3(i, b, e) F_OR(i, b, e, 1)
+#define EACH(x, a) for (auto& x : a)
+
+using namespace std;
+
+template<typename _Tp>
+    struct compare : public binary_function<_Tp, _Tp, bool>
+    {
+      _GLIBCXX14_CONSTEXPR
+      bool
+      operator()(const _Tp& __x, const _Tp& __y) const
+      {
+        if(abs(__x) != abs(__y)) return abs(__x) > abs(__y);
+        return __x > __y;
+      }
+    };
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    priority_queue<int, vt<int>, compare<int>> heap;
+
+    int n;
+    cin >> n;
+
+    int input;
+    while(n--) {
+        cin >> input;
+        if(input) {
+            heap.push(input);
+        }
+
+        else {
+            if(heap.empty()) {
+                cout << "0\n";
+                continue;
+            }
+            cout << heap.top() << '\n';
+            heap.pop();
+        }
+    }
+}
