c3ff65010b0e1c0126188d4d904431ae0b27c206
MilkshakeCake <olleme@gmail.com>
2022-10-12 15:57:10 +0900

    221012 9251 complete commit

--- 3.Gold/1000~9999/9251/9251.cpp
+++ 3.Gold/1000~9999/9251/9251.cpp
@@ -30,10 +30,25 @@
 
 using namespace std;
 
+int dp[1001][1001] = {};
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
     
-    
+    string a;
+    string b;
+    cin >> a >> b;
+
+    for(int i = 1; i <= sz(a); i++) {
+        for(int j = 1; j <= sz(b); j++) {
+            if(a[i -1] == b[j -1]) {
+                dp[i][j] = dp[i -1][j -1] + 1;
+            }
+            else dp[i][j] = max(dp[i -1][j], dp[i][j -1]);
+        }
+    }
+
+    cout << dp[sz(a)][sz(b)];
 }
