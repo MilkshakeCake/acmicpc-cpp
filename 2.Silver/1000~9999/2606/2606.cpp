209a4808fc734984b96ca037abfc33e004880075
MilkshakeCake <olleme@gmail.com>
2022-10-18 15:14:55 +0900

    221018 2606 complete commit

---
+++ 2.Silver/1000~9999/2606/2606.cpp
@@ -0,0 +1,66 @@
+// 바이러스 - Baekjoon Online Judge #2606
+
+#include <string>
+#include <cmath>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <utility>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <fstream>
+#include <sstream>
+#include <map>
+
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define fr first
+#define sc second
+#define vt vector
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
+
+#define F_OR(i, a, b, s) for (int i = (a); (s) > 0 ? i < (b) : i > (b); i += (s))
+#define F_OR1(e) F_OR(i, 0, e, 1)
+#define F_OR2(i, e) F_OR(i, 0, e, 1)
+#define F_OR3(i, b, e) F_OR(i, b, e, 1)
+#define EACH(x, a) for (auto& x : a)
+
+using namespace std;
+
+vector<int> graph[100001];
+int visited[100001] = {0, };
+int cnt = 0;
+
+void dfs(int r) {
+    if(visited[r]) return;
+    visited[r] = true;
+    cnt++;
+
+    for(int i = 0; i < sz(graph[r]); i++) {
+        dfs(graph[r][i]);
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int n, m;
+    cin >> n >> m;
+    for(int i = 0; i < m; i++) {
+        int a, b;
+        cin >> a >> b;
+        graph[a].push_back(b);
+        graph[b].push_back(a);
+    }
+
+    dfs(1);
+
+    cout << cnt -1;
+}
