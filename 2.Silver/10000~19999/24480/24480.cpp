6bde99cb332c81b5b57758c9228d5261913aed77
MilkshakeCake <olleme@gmail.com>
2022-10-18 10:58:13 +0900

    221018 24480 complete commit

---
+++ 2.Silver/10000~19999/24480/24480.cpp
@@ -0,0 +1,71 @@
+// "Project_Name" - Baekjoon Online Judge #
+
+#include <string>
+#include <cmath>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <utility>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <fstream>
+#include <sstream>
+#include <map>
+
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define fr first
+#define sc second
+#define vt vector
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
+
+#define F_OR(i, a, b, s) for (int i = (a); (s) > 0 ? i < (b) : i > (b); i += (s))
+#define F_OR1(e) F_OR(i, 0, e, 1)
+#define F_OR2(i, e) F_OR(i, 0, e, 1)
+#define F_OR3(i, b, e) F_OR(i, b, e, 1)
+#define EACH(x, a) for (auto& x : a)
+
+using namespace std;
+
+vector<int> graph[100001];
+int visited[100001] = {0, };
+int result[100001];
+int cnt = 0;
+
+void dfs(int r) {
+    if(visited[r]) return;
+
+    visited[r] = true;
+    result[r] = ++cnt;
+
+    for(int i = 0; i < sz(graph[r]); i++) {
+        dfs(graph[r][i]);
+    }
+}
+
+int main() {
+    int n, m, r;
+    scanf("%d %d %d", &n, &m, &r);
+
+    for(int i = 1; i <= m; i++) {
+        int s, f;
+        scanf("%d %d", &s, &f);
+        graph[s].push_back(f);
+        graph[f].push_back(s);
+    }
+    
+    for(int i = 1; i <= n; i++) {
+        sort(all(graph[i]), greater<>());
+    }
+    
+    dfs(r);
+
+    for(int i = 1; i <= n; i++) {
+        cout << result[i] << '\n';
+    }
+}
