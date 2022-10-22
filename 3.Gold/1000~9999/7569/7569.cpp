313eda11811a866fb01da8cc6372ee1c03805d3e
MilkshakeCake <ollehme@gmail.com>
2022-10-22 17:00:59 +0900

    221022 7569 complete commit

--- 3.Gold/1000~9999/7569/7569.cpp
+++ 3.Gold/1000~9999/7569/7569.cpp
@@ -33,25 +33,43 @@
 
 using namespace std;
 
-struct Coordinate {
-    int x;
-    int y;
-    int z;
-
-    Coordinate(int xc, int yc, int zc) : x(xc), y(yc), z(zc) {};
-};
-
-vt<vt<vt<Coordinate>>> dp;
 vt<vt<vt<int>>> boxes;
-vt<Coordinate> ripe;
-int needripe = 0;
+vt< vt<int> > ripe;
+vt< vt<int> > moves = {{1, 0, 0}, {-1, 0, 0}, {0, 1, 0}, {0, -1, 0}, {0, 0, 1}, {0, 0, -1}};
+vt<int> last;
+int m, n, h, needripe(0);
 
 void init() {
-    vt<vt<Coordinate>> temp(101, vt<Coordinate>(101));
-    vt<vt<int>> tmp(101, vt<int>(101));
-    for(int i = 0; i < 101; i++) {
-        dp.push_back(temp);
-        boxes.push_back(tmp);
+    vt<vt<int>> box(102, vt<int>(102, -1));
+    vt<vt<vt<int>>> step2;
+    for(int i = 0; i < 102; i++) {
+        boxes.push_back(box);
+    }
+}
+
+void bfs() {
+    queue<vt<int>> que;
+    for(auto& i : ripe) {
+        que.push({i[0], i[1], i[2]});
+        last = i;
+    }
+
+    while(!que.empty()) {
+        vt<int> v = que.front();
+        que.pop();
+        for(auto& i : moves) {
+            int X = v[0] + i[0];
+            int Y = v[1] + i[1];
+            int Z = v[2] + i[2];
+
+            if(X <= 0 || Y <= 0 || Z <= 0 || X > n || Y > m || Z > h) continue;
+            if(boxes[X][Y][Z] != 0) continue;
+
+            que.push({X, Y, Z});
+            boxes[X][Y][Z] = boxes[v[0]][v[1]][v[2]] +1;
+            last = {X, Y, Z};
+            needripe--;
+        }
     }
 }
 
@@ -60,23 +78,35 @@ int main() {
     cin.tie(NULL);
     cout.tie(NULL);
 
-    Coordinate Co(0, 0, 0);
-    
-    int m, n, h;
-    cin >> m >> n >> h;
-    for(int i = 0; i < h; i++) {
-        for(int j = 0; j < m; j++) {
-            for(int k = 0; k < n; k++) {
-                int temp;
-                cin >> temp;
-                boxes[i][j][k] = temp;
-                if(temp == 1) {
-                    Co.x = i;
-                    Co.y = j;
-                    Co.z = k;
-                    ripe.push_back(Co);
+    init();
+
+    int temp;
+
+    scanf("%d %d %d", &m, &n, &h);
+    for(int i = 1; i <= h; i++) {
+        for(int j = 1; j <= n; j++) {
+            for(int k = 1; k <= m; k++) {
+                scanf("%d", &temp);
+                boxes[j][k][i] = temp;
+                if(temp == 1) ripe.push_back({j, k, i});
+                else if(temp == 0) {
+                    needripe++;
                 }
             }
         }
     }
+
+    if(needripe == 0) {
+        printf("0");
+        return 0;
+    }
+
+    bfs();
+
+    if(needripe != 0) {
+        printf("-1");
+        return 0;
+    }
+
+    printf("%d", boxes[last[0]][last[1]][last[2]] -1);
 }
