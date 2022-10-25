496e68a276ed98075199e12ded51b48fcd0bd833
MilkshakeCake <ollehme@gmail.com>
2022-10-25 17:06:10 +0900

    221025 851-M complete commit

--- CodeTest/851/M/M.cpp
+++ CodeTest/851/M/M.cpp
@@ -14,25 +14,31 @@
 
 using namespace std;
 
-int play[100000];
+vector<long long> play(100000, 0);
+bool prev[100000] = {false};
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int n;
-    int sum = 0;
-    int maxn = 0;
+    long long n;
+    long long sum = 0;
     cin >> n;
     for(int i = 0; i < n; i++) {
         cin >> play[i];
         sum += play[i];
-        maxn = max(maxn, play[i]);
     }
 
-    sum -= maxn;
+    sort(play.begin(), play.end(), greater<>());
 
-    long long res = min(sum +1, maxn) + sum;
-    cout << res;
+    if(play[0] > (sum - play[0] + 1)) {
+        cout << ((sum - play[0]) * 2) + 1;
+    }
+
+    else {
+        cout << sum;
+    }
 }
+
+// 숫자 크다 싶으면 long long 쓰기
