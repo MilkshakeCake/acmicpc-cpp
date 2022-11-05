e960422e59ecfbf04381b9818bf087772d230f7c
MilkshakeCake <ollehme@gmail.com>
2022-11-05 16:03:44 +0900

    221105 7662 complete

---
+++ 3.Gold/1000~9999/7662.cpp
@@ -0,0 +1,72 @@
+// 이중 우선순위 큐 - Baekjoon Online Judge #7662
+// Dual Priority Queue
+
+//입력
+#include <iostream>
+#include <fstream>
+#include <sstream>
+//함수
+#include <algorithm>
+#include <utility>
+#include <cmath>
+//자료구조
+#include <queue>
+#include <stack>
+#include <deque>
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
+
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define fr first
+#define sc second
+#define vt vector
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
+
+#define F_OR(i, a, b, s) for (int i = (a); (s) > 0 ? i < (b) : i > (b); i += (s))
+#define F_OR1(e) F_OR(i, 0, e, 1)
+#define F_OR2(i, e) F_OR(i, 0, e, 1)
+#define F_OR3(i, b, e) F_OR(i, b, e, 1)
+#define EACH(x, a) for (auto& x : a)
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int T;
+    cin >> T;
+    while(T--) {
+        int k, num;
+        char input;
+        multiset<int> ms;
+
+        cin >> k;
+        while(k--) {
+            cin >> input >> num;
+            if(input == 'I') ms.insert(num);
+            else if(!ms.empty()) {
+                if(num == 1) {
+                    ms.erase(--ms.end());
+                }
+                else {
+                    ms.erase(ms.begin());
+                }
+            }
+        }
+        if(ms.empty()) {
+            cout << "EMPTY" << '\n';
+        }
+        else {
+            cout << *(--ms.end()) << ' ' << *ms.begin() << '\n';
+        }
+    }
+}
