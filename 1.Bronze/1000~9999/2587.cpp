492b9c31eb0971cf46b1beab0a7de87e1e3609b7
MilkshakeCake <ollehme@gmail.com>
2022-11-14 22:25:01 +0900

    221114 2587 complete

---
+++ 1.Bronze/1000~9999/2587.cpp
@@ -0,0 +1,39 @@
+// 대표값2 - Baekjoon Online Judge #2587
+
+#include <bits/stdc++.h>
+
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define fr first
+#define sc second
+#define vt vector
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
+
+#define F_OR(i, a, b, s) for (int i = (a); (s) > 0 ? i < (b) : i > (b); i += (s))
+#define F_OR1(e) F_OR(i, 0, e, 1)
+#define F_OR2(i, e) F_OR(i, 0, e, 1)
+#define F_OR3(i, b, e) F_OR(i, b, e, 1)
+#define EACH(x, a) for (auto& x : a)
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int temp, sum = 0;
+    vt<int> num;
+    for(int i = 0; i < 5; i++) {
+        cin >> temp;
+        num.push_back(temp);
+        sum += temp;
+    }
+
+    sort(all(num));
+    cout << sum / 5 << '\n' << num[2];
+}
