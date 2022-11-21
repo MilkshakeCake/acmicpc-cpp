100c7e13277a1929c5ce6d114c3808b869f334df
MilkshakeCake <olleme@gmail.com>
2022-11-21 15:11:22 +0900

    221121 26005 complete

---
+++ 2.Silver/20000-29999/26005.cpp
@@ -0,0 +1,38 @@
+// 나뭇잎 학회 - Baekjoon Online Judge #26005
+// BOJ Contest 884 - 홍익대학교 PS 동아리 HI-ARC 2022
+
+#include <bits/stdc++.h>
+
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define fr first
+#define sc second
+#define vt vector
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
+
+#define F_OR(i, a, b, s) for (int i = (a); (s) > 0 ? i < (b) : i > (b); i += (s))
+#define F_OR1(e) F_OR(i, 0, e, 1)
+#define F_OR2(i, e) F_OR(i, 0, e, 1)
+#define F_OR3(i, b, e) F_OR(i, b, e, 1)
+#define EACH(x, a) for (auto& x : a)
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int n; cin >> n;
+    if(n == 1) cout << 0;
+    else if(n % 2) {
+        cout << (n * n -1) / 2 + 1;
+    }
+    else {
+        cout << n * n / 2;
+    }
+}
