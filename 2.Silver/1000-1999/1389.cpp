96a220d57df7b9669121d1c972ffa9df4af3fe5f
MilkshakeCake <olleme@gmail.com>
2022-11-23 11:29:04 +0900

    221123 1389 complete

---
+++ 2.Silver/1000-1999/1389.cpp
@@ -0,0 +1,79 @@
+// 케빈 베이컨의 6단계 법칙 - Baekjoon Online Judge #1389
+
+#include <bits/stdc++.h>
+
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define fr first
+#define sc second
+#define vt vector
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
+
+#define F_OR(i, a, b, s) for (int i = (a); (s) > 0 ? i < (b) : i > (b); i += (s))
+#define F_OR1(e) F_OR(i, 0, e, 1)
+#define F_OR2(i, e) F_OR(i, 0, e, 1)
+#define F_OR3(i, b, e) F_OR(i, b, e, 1)
+#define EACH(x, a) for (auto& x : a)
+
+using namespace std;
+
+vt<int> dist[101];
+vt<int> graph[101];
+
+void dijkstra(int start) {
+    vt<int> cache(101, -1);
+    cache[start] = 0;
+
+    queue<int> que;
+    que.push(start);
+    while(!que.empty()) {
+        int now = que.front();
+        que.pop();
+
+        for(auto &i : graph[now]) {
+            if(cache[i] != -1) continue;
+            cache[i] = cache[now] +1;
+            que.push(i);
+        }
+    }
+
+    dist[start] = cache;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int n, m;
+    cin >> n >> m;
+    
+    while(m--) {
+        int u, v;
+        cin >> u >> v;
+        graph[u].push_back(v);
+        graph[v].push_back(u);
+    }
+
+    int minMem = 0;
+    int kevin = 5555;
+
+    for(int i = 1; i <= n; i++) {
+        dijkstra(i);
+        dist[i][0] = 0;
+        for(int j = 1; j <= n; j++) {
+            dist[i][0] += dist[i][j];
+        }
+
+        if(dist[i][0] < kevin) {
+            minMem = i;
+            kevin = dist[i][0];
+        }
+    }
+
+    cout << minMem;
+}
