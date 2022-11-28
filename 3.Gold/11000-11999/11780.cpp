17cb02cbe86255f7c7649658fe99e7f01e1ccf43
MilkshakeCake <olleme@gmail.com>
2022-11-28 09:32:23 +0900

    221128 file trim

---
+++ 3.Gold/11000-11999/11780.cpp
@@ -0,0 +1,25 @@
+// 플로이드 2 - Baekjoon Online Judge #11780
+
+#include <bits/stdc++.h>
+
+typedef long long ll;
+typedef unsigned long long ull;
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define fr first
+#define sc second
+#define vt vector
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
+#define printall(i, a) for (auto &i : a) cout << i << ' '
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    
+}
