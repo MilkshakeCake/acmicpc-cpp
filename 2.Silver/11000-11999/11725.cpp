08be4000ccd772b456eed4c484a7d8a180eeae3c
MilkshakeCake <olleme@gmail.com>
2022-11-28 15:09:38 +0900

    221128 11725 complete

---
+++ 2.Silver/11000-11999/11725.cpp
@@ -0,0 +1,53 @@
+// 트리의 부모 찾기 - Baekjoon Online Judge #11725
+
+#include <bits/stdc++.h>
+
+typedef long long ll;
+typedef unsigned long long ull;
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define fr first
+#define sc second
+#define vt vector
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
+#define printall(i, a) for (auto &i : a) cout << i << ' '
+
+using namespace std;
+
+vt<int> tree[100001];
+vt<int> parent(100001, 0);
+
+void dfs(int k) {
+    for(auto i : tree[k]) {
+        if(!parent[i]) {
+            parent[i] = k;
+            dfs(i);
+        }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int n;
+    cin >> n;
+
+
+    for(int i = 1; i < n; i++) {
+        int a, b;
+        cin >> a >> b;
+        tree[a].push_back(b);
+        tree[b].push_back(a);
+    }
+
+    parent[1] = 1;
+    dfs(1);
+
+    for(int i = 2; i <= n; i++) {
+        cout << parent[i] << '\n';
+    }
+}
