7e2140646be6e42c514ce061c30281f2e2852796
MilkshakeCake <ollehme@gmail.com>
2022-12-02 13:18:52 +0900

    221202 5639 complete

--- 3.Gold/3000~9999/5639.cpp
+++ 3.Gold/3000~9999/5639.cpp
@@ -18,14 +18,42 @@ using namespace std;
 
 struct Node {
     int val, left, right;
-    Node() : Node(0, 0, 0) {}
-    Node(int n, int l, int r) : val(n), left(l), right(r) {}
 };
+
+vt<Node> tree;
+int root, num, cnt(0);
+bool flag;
+
+void plant(int place, int idx) {
+	if(tree[place].val > tree[idx].val) {
+		if(tree[place].left == 0) tree[place].left = idx;
+		else plant(tree[place].left, idx);
+	}
+	else {
+		if(tree[place].right == 0) tree[place].right = idx;
+		else plant(tree[place].right, idx);
+	}
+}
+
+void postOrder(int now){
+	if(tree[now].left != 0) postOrder(tree[now].left);
+	if(tree[now].right != 0) postOrder(tree[now].right);
+	cout << tree[now].val << '\n';
+}
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
     
-    
+    cin >> num;
+    tree.push_back({num, 0, 0});
+
+    while(cin >> num) {
+        cnt++;
+        tree.push_back({num, 0, 0});
+        plant(0, cnt);
+    }
+
+    postOrder(0);
 }
