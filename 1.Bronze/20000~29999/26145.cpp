50fd487a0fee10e7323019c96697c4182f0e2ac2
MilkshakeCake <ollehme@gmail.com>
2022-12-06 17:02:32 +0900

    221206 26146 complete

---
+++ 1.Bronze/20000~29999/26145.cpp
@@ -0,0 +1,41 @@
+// 출제비 재분배 - BOJ #26145
+
+#include <bits/stdc++.h>
+
+typedef long long ll;
+typedef unsigned long long ull;
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define fr first
+#define sc second
+#define vt vector
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
+#define printall(i, a) for (auto &i : a) cout << i << ' '
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int n, m, temp;
+    cin >> n >> m;
+    vt<int> csuite(n + m, 0);
+    for(int i = 0; i < n; i++) {
+        cin >> temp;
+        csuite[i] = temp;
+    }
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n + m; j++) {
+            cin >> temp;
+            csuite[i] -= temp;
+            csuite[j] += temp;
+        }
+    }
+
+    printall(i, csuite);
+}
---
+++ 4.Platinum/26146.cpp
@@ -0,0 +1,58 @@
+// 즉흥 여행 - BOJ #26146
+// SCC(Strongly Connected Components)
+
+#include <bits/stdc++.h>
+
+typedef long long ll;
+typedef unsigned long long ull;
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define fr first
+#define sc second
+#define vt vector
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
+#define printall(i, a) for (auto &i : a) cout << i << ' '
+
+using namespace std;
+
+int N, M;
+bool flag = true;
+vt<int> graph[200001];
+vt<int> rev[200001];
+vt<int> scc;
+stack<int> stk;
+vt<bool> visited(200001, false);
+
+void dfs(int now, bool reverse) {
+    if(visited[now]) return;
+    visited[now] = true;
+
+    for(auto &i : (reverse ? rev[now] : graph[now])) if(!visited[i]) dfs(i, reverse);
+    reverse ? scc.push_back(now) : stk.push(now);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    cin >> N >> M;
+    int v, w;
+    while(M--) {
+        cin >> v >> w;
+        graph[v].push_back(w);
+        rev[w].push_back(v);
+    }
+
+    for(int i = 1; i <= N; i++) {
+        dfs(i, false);
+    }
+
+    visited.assign(200001, false);
+
+    dfs(stk.top(), true);
+
+    cout << (sz(scc) == N ? "Yes" : "No");
+}
