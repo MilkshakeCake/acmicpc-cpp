94725e454e4b0d62b8f7d36c0a64890d12b98629
MilkshakeCake <ollehme@gmail.com>
2022-12-19 18:16:13 +0900

    221219 2467 complete

---
+++ 3.Gold/2000-2999/2467.cpp
@@ -0,0 +1,48 @@
+// 용액 - BOJ #2467
+
+#include <bits/stdc++.h>
+
+typedef long long ll;
+typedef unsigned long long ull;
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define fr first
+#define sc second
+#define vt vector
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
+#define printall(i, a) for (auto &i : a) cout << i << ' '
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int n, temp; cin >> n;
+    vt<int> liquid;
+    for(int i = 0; i < n; i++) {
+        cin >> temp;
+        liquid.push_back(temp);
+    }
+    sort(all(liquid));
+
+    int start(0), end(n -1);
+    int alk(liquid[start]), acid(liquid[end]);
+    int mix = INT32_MAX;
+    while(start < end) {
+        int sum = liquid[start] + liquid[end];
+        if(abs(sum) < mix) {
+            mix = abs(sum);
+            alk = liquid[start];
+            acid = liquid[end];
+
+            if(sum == 0) break;
+        }
+        sum < 0 ? start++ : end--;
+    }
+
+    cout << alk << ' ' << acid;
+}
