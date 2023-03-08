ca682ecc39dcb2683ab927643df8ee4663ae2662
MilkshakeCake <ollehme@gmail.com>
2023-03-08 19:11:41 +0900

    230308 20040 comp

--- 3.Gold/20000-29999/20040.cpp
+++ 3.Gold/20000-29999/20040.cpp
@@ -16,10 +16,49 @@ typedef long double ld;
 
 using namespace std;
 
+vt<int> root(500001, 0);
+vt<bool> isNew(500001, true);
+
+int find(int node) {
+    if(root[node] == node) return node;
+    return find(root[node]);
+}
+
+bool uni(int a, int b) {
+    int ra = find(a);
+    int rb = find(b);
+
+    if(ra == rb) return true;
+
+    root[rb] = ra;
+    return false;
+}
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
     
-    
+    int n, m;
+    cin >> n >> m;
+
+    int a, b, result = 0;
+    for(int i = 1; i <= m; i++) {
+        cin >> a >> b;
+        if(isNew[a]) {
+            root[a] = a;
+            isNew[a] = false;
+        }
+        if(isNew[b]) {
+            root[b] = b;
+            isNew[b] = false;
+        }
+        
+        if(uni(a, b)) {
+            result = i;
+            break;
+        }
+    }
+
+    cout << result;
 }
